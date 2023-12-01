51d9df544ee680e67673481790cadf5123d1221c
Kevin Tourret <kevin@drosalys.fr>
2023-12-01 17:02:09 +0100

    correction 3rd exo

--- src/main/java/fr/ktourret/poec/Main.java
+++ src/main/java/fr/ktourret/poec/Main.java
@@ -8,11 +8,32 @@ import java.util.GregorianCalendar;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(exo1());
+        System.out.println(exo1(42));
+        System.out.println(exo2Hello("Kevin"));
+        System.out.println(exo3("Kevin", 5));
     }
 
-    public static int exo1() {
-        return 1;
+    public static int exo1(int age) {
+        Calendar calendar = new GregorianCalendar();
+        calendar.setTime(new Date());
+        return calendar.get(Calendar.YEAR) - age;
+    }
+
+    public static String exo2Hello(String name) {
+        if (name == null) {
+            return null;
+        }
+        return "Bonjour, " + name + " ton nom contient " + name.length() + " lettres";
+    }
+
+    public static String exo3(String name, int position) {
+        if (name == null) {
+            return null;
+        }
+        if (name.length() < position || position < 0) {
+            return null;
+        }
+        return "La lettre à la position " + position + " dans " + name + " est " + name.charAt(position - 1);
     }
 
 }
